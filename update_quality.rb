8e3478546aeb4fa6e06ee010161c8939e3dc1890
Ariel Krakowski <arikrak@gmail.com>
2014-11-28 14:50:12 -0500

    without changing award

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,58 @@
 require 'award'
 
+# Use this code if not allowed to change Award. 
+
+# runs each day
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
+    next if unchanging(award)
+
+    change = award_change(award)
+    if award.expires_in <= 0
+      change = expire_adjustment(award, change)
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+
+    award.quality += change
+
+    #adjust for mins & maxes
+    award.quality = [award.quality, 50].min
+    award.quality = [award.quality, 0].max
+
+    award.expires_in -= 1
   end
+end
+
+
+def award_change(award)
+  case award.name
+    when 'Blue First'
+     1
+    when 'Blue Compare'
+      case award.expires_in  
+      when 6..10
+        2
+      when 1..5
+        3
+      else
+        1
+      end      
+    when 'Blue Star'
+      -2
+    else # Normal award
+      -1
+  end
+end
+
+# adjust after expiration
+def expire_adjustment(award, change)
+  case award.name
+    when 'Blue Compare'
+      -award.quality
+    else
+      change * 2
+  end
+end
+
+def unchanging(award)
+  award.name == 'Blue Distinction Plus'
 end
